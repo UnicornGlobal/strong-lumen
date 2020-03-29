b08ce92150356d80b4a8618504549240befeeaf9
Moondas <peter.vigh@stylersonline.com>
2020-03-29 22:09:49 +0200

    Update CORS config

--- config/cors.php
+++ config/cors.php
@@ -2,16 +2,43 @@
 
 return [
     /*
-     | Laravel CORS
-     |
-     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
-     | to accept any value.
-     |
+    * You can enable CORS for 1 or multiple paths.
+    * Example: ['api/*']
+    */
+    'paths' => [],
+
+    /*
+    * Matches the request method. `[*]` allows all methods.
+    */
+    'allowed_methods' => ['GET', 'POST', 'PUT',  'DELETE', 'OPTIONS'],
+
+    /*
+     * Matches the request origin. `[*]` allows all origins.
      */
-    'supportsCredentials' => true,
-    'allowedOrigins'      => ['*'],
-    'allowedHeaders'      => ['Content-Type', 'Content-Length', 'Origin', 'X-Requested-With', 'Debug-Token', 'Registration-Access-Key', 'X-CSRF-Token', 'App', 'User-Agent', 'Authorization'],
-    'allowedMethods'      => ['GET', 'POST', 'PUT',  'DELETE', 'OPTIONS'],
-    'exposedHeaders'      => ['Authorization'],
-    'maxAge'              => 0,
+    'allowed_origins' => ['*'],
+
+    /*
+     * Matches the request origin with, similar to `Request::is()`
+     */
+    'allowed_origins_patterns' => [],
+
+    /*
+     * Sets the Access-Control-Allow-Headers response header. `[*]` allows all headers.
+     */
+    'allowed_headers' => ['Content-Type', 'Content-Length', 'Origin', 'X-Requested-With', 'Debug-Token', 'Registration-Access-Key', 'X-CSRF-Token', 'App', 'User-Agent', 'Authorization'],
+
+    /*
+     * Sets the Access-Control-Expose-Headers response header.
+     */
+    'exposed_headers' => ['Authorization'],
+
+    /*
+     * Sets the Access-Control-Max-Age response header.
+     */
+    'max_age' => 0,
+
+    /*
+     * Sets the Access-Control-Allow-Credentials header.
+     */
+    'supports_credentials' => true,
 ];
